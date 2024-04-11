d122c51d733238d98dc263a93442d8bb3a028c87
Vishnuveera19 <vishnuveeraraghavan1905@gmail.com>
2024-04-11 15:47:55 +0530

    all files

---
+++ src/App.js
@@ -0,0 +1,111 @@
+
+import './App.css';
+import EarnDeductForm from './components/EarnDeductForm';
+import DailyTimeCardForm from './components/DailyTimeCardForm';
+import './components/attendenceform';
+import AttendenceForm from './components/attendenceform';
+import './components/sampleform';
+import SampleForm from './components/sampleform';
+import SampleForm7 from './components/form7form'; 
+import HrAuthForm from './components/HrAuthenticationForm'; 
+import CourseForm from './components/HrmmCourseForm'; 
+import SkillsMasterForm from './components/HrmmSkillsMasterForm';
+import SpecializationForm from './components/HrmmSpecializationForm';
+import LeaveAppForm from './components/LeaveApplyForm';
+import LeaveSettleForm from './components/LeaveSettlementForm';
+import LeaveAllocationForm from './components/LeaveAllocationMasterForm';
+
+import ApprovehrForm  from './components/LeaveApproveHrForm';
+
+import ApproveManagerForm from './components/LeaveApproveManagerForm';
+
+import LoanPostForm from './components/LoanPostForm';
+import LoanPreclosForm from './components/LoanPreclosureForm';
+import LoanEntForm from './components/LoanEntryForm';
+import OTSlabForm from './components/OTSLabForm';
+import PayinputForm from './components/PayInputForm';
+import PaymattbonusForm from './components/PaymAttBonusForm';
+import OndutyForm from './components/ondutyForm';
+import PaybankForm from './components/paymbankForm';
+import PayBranchForm from './components/PaymBranchForm';
+import CarryForwardForm from './components/paymCarryForwardForm';
+import CategoryForm from './components/PaymCategoryForm';
+import CompanyForm from './components/paymCompanyForm';
+import ComputationForm from './components/paymComputationForm';
+import DepartmentForm from './components/paymDepartmentForm';
+import DesignationForm from './components/PaymDesignationForm';
+import DivisionForm from './components/paymDivisionForm';
+import EarningsForm from './components/paymEarningsForm';
+import EmpDeductionForm from './components/paymEmpDeductionForm';
+import EmpEarningsForm from './components/paymEmpEarningsForm';
+import EmployeeForm from './components/paymEmployeeForm';
+import EmployeeLeaveForm from './components/paymEmployeeLeaveForm';
+import EmployeeProfile1Form from './components/paymEmployeeProfile1Form';
+import EmployeeWorkDetailsForm from './components/paymEmployeeWorkDetailsForm';
+import EncashmentDetailsForm from './components/paymEncashmentDetailsForm';
+import GradeForm from './components/paymGradeForm';
+import HolidayForm from './components/paymHolidaysForm';
+import JobStatusForm from './components/paymJobStatusForm';
+import LeaveForm from './components/paymleaveForm';
+import LeaveAllocation1Form from './components/paymleaveAllocation1Form';
+import LevelForm from './components/paymLevelForm';
+import DeductionForm from './components/paymDeductionForm';
+import ShiftDetailsForm from './components/tempshiftdetailsForm';
+
+
+function App() {
+  return (
+    <div >
+      {/* <SampleForm/> */}
+        {/* <DailyTimeCardForm/>  */}
+         {/* <SampleForm7/> */}
+       {/* <EarnDeductForm/> */}
+       {/* <HrAuthForm/> */}
+      {/* <CourseForm/> */}
+      {/* <SkillsMasterForm/> */}
+      {/* <SpecializationForm/> */}
+     {/* <LeaveAppForm/> */}
+     {/* <LeaveSettleForm/> */}
+     {/* <LeaveAllocationForm/> */}
+    {/* <ApprovehrForm/> */}
+{/* <AttendenceForm/> */}
+{/* <ApproveManagerForm/> */}
+{/* <SampleForm7/> */}
+{/* <LoanPostForm/> */}
+
+{/* <LoanPreclosForm /> */}
+{/* <LoanEntForm /> */}
+{/* <OTSlabForm/> */}
+{/* <PayinputForm/> */}
+{/* <PaymattbonusForm/> */}
+{/* <OndutyForm/> */}
+{/* <PaybankForm/> */}
+{/* <PaybankForm/> */}
+{/* <CarryForwardForm/> */}
+{/* <CategoryForm/> */}
+{/* <CompanyForm/> */}
+{/* <ComputationForm/> */}
+{/* <DepartmentForm/> */}
+{/* <DesignationForm/> */}
+{/* <DivisionForm/> */}
+{/* <EarningsForm/> */}
+{/* <EmpDeductionForm/> */}
+{/* <EmpEarningsForm/> */}
+{/* <EmployeeForm/> */}
+{/* <EmployeeLeaveForm/> */}
+{/* <EmployeeProfile1Form/> */}
+{/* <EmployeeWorkDetailsForm/> */}
+{/* <EncashmentDetailsForm/> */}
+{/* <GradeForm/> */}
+{/* <HolidayForm/> */}
+{/* <JobStatusForm/> */}
+{/* <LeaveForm/> */}
+{/* <LeaveAllocation1Form/> */}
+{/* <LevelForm/> */}
+{/* <DeductionForm/> */}
+<ShiftDetailsForm/>
+</div>
+  );
+}
+
+export default App;
